600e412f62991f6f7a0d8af7995a71eae562bcc6
baikangwang <baikangwang@hotmail.com>
2013-09-26 03:22:00 +0000

    Windows Service host WCF

---
+++ WCFSample/WCFSample.WS.Basic.Service/CalculatorService.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WCFSample.Contract;
+
+namespace WCFSample.WS.Basic.Service
+{
+    public class CalculatorService:CalculatorBase
+    {
+    }
+}
---
+++ WCFSample/WCFSample.WS.Basic.Service/WindowsCalculatorService.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceModel;
+using System.ServiceProcess;
+using System.Text;
+
+namespace WCFSample.WS.Basic.Service
+{
+    public partial class WindowsCalculatorService : ServiceBase
+    {
+        public ServiceHost ServiceHost = null;
+        
+        public WindowsCalculatorService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+            if(ServiceHost!=null)
+                ServiceHost.Close();
+
+            ServiceHost=new ServiceHost(typeof(CalculatorService));
+
+            ServiceHost.Open();
+        }
+
+        protected override void OnStop()
+        {
+            if (ServiceHost != null)
+            {
+                ServiceHost.Close();
+                ServiceHost = null;
+            }
+        }
+    }
+}
