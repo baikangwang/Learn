e47a38fb5c0a67b4fb716e277fb2089fb78fe751
baikangwang <baikangwang@hotmail.com>
2013-10-18 05:16:24 +0000

    Initial check in

---
+++ RegulexTest/RegulexTest/Test.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text.RegularExpressions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RegulexTest
+{
+    [TestClass]
+    public class Test
+    {
+        [TestMethod]
+        public void XmlTest()
+        {
+            string xml = @"<root><Name> ddd df*&&<>$%^%)()(dfd </Name></root>";
+            //string pattern = @"<Name[^>]*>(?<value>.*?)\k<value></Name>";
+            string pattern = @"<Name[^>]*>(?<value>.*)</Name>";
+            MatchCollection matches = Regex.Matches(xml, pattern);
+            
+            Console.WriteLine("Match Count: {0}",matches.Count);
+            
+            
+            
+            foreach (Match match in matches)
+            {
+                //Console.WriteLine("Match Count: {0}", matches.);
+
+                foreach (Group g in match.Groups)
+                {
+                    Console.WriteLine(string.Format("Group {0}:{1}, Succesful:{2}",g.Index,g.Value,g.Success));
+                }
+            }
+            Group group = matches[0].Groups["value"];
+            Console.WriteLine(string.Format("Group {0}:{1}, Succesful:{2}", group.Index, group.Value, group.Success));
+        }
+    }
+}
---
+++ XMLValidation/XMLValidation/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.XPath;
+
+namespace XMLValidation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                XmlReaderSettings settings = new XmlReaderSettings();
+                settings.Schemas.Add("http://www.contoso.com/books", "contosoBooks.xsd");
+                settings.ValidationType = ValidationType.Schema;
+                settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessInlineSchema;
+                settings.ValidationFlags |= XmlSchemaValidationFlags.ProcessSchemaLocation;
+                settings.ValidationFlags |= XmlSchemaValidationFlags.ReportValidationWarnings;
+                settings.ValidationEventHandler += ValidationEventHandler;
+
+                XmlReader reader = XmlReader.Create("contosoBooks.xml", settings);
+                //XmlDocument document = new XmlDocument();
+                //document.Load(reader);
+                while (reader.Read())
+                {
+                    
+                }
+
+                //ValidationEventHandler eventHandler = new ValidationEventHandler(ValidationEventHandler);
+
+                // the following call to Validate succeeds.
+                //document.Validate(eventHandler);
+
+                // add a node so that the document is no longer valid
+                //XPathNavigator navigator = document.CreateNavigator();
+                //navigator.MoveToFollowing("price", "http://www.contoso.com/books");
+                //XmlWriter writer = navigator.InsertAfter();
+                //writer.WriteStartElement("anotherNode", "http://www.contoso.com/books");
+                //writer.WriteEndElement();
+                //writer.Close();
+
+                // the document will now fail to successfully validate
+                //document.Validate(eventHandler);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Read();
+        }
+
+        static void ValidationEventHandler(object sender, ValidationEventArgs e)
+        {
+            switch (e.Severity)
+            {
+                case XmlSeverityType.Error:
+                    Console.WriteLine("Error (line {1} Col {2}): {0}", e.Message,e.Exception.LineNumber,e.Exception.LinePosition);
+                    break;
+                case XmlSeverityType.Warning:
+                    Console.WriteLine("Warning (line {1} Col {2}): {0}", e.Message,e.Exception.LineNumber,e.Exception.LinePosition);
+                    break;
+            }
+        }
+    }
+}
